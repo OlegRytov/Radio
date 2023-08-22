5618652cfe8b7ea456209d274d512015ab75e65e
Oleg Rytov <cattimofei@gmail.com>
2023-08-22 11:35:34 +0300

    edit Project

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -15,19 +15,56 @@ public class Radio {
     private int minVolume = 0;
     private int currentVolume;
 
-    public Radio(int currentRadioStation) {
-        this.maxRadioStation = currentRadioStation - 1;
+    public int getMaxRadioStation() {
+        return maxRadioStation;
     }
 
+    public void setMaxRadioStation(int maxRadioStation) {
+        this.maxRadioStation = maxRadioStation - 1;
+    }
+
+    public int getMinRadioStation() {
+        return minRadioStation;
+    }
+
+    public void setMinRadioStation(int minRadioStation) {
+        this.minRadioStation = minRadioStation;
+    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
     }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+    public Radio(int maxRadioStation) {
+        this.maxRadioStation = maxRadioStation - 1;
+    }
     public void setCurrentRadioStation(int newCurrentRadioStation) {
         if (newCurrentRadioStation < minRadioStation) {
             return;
